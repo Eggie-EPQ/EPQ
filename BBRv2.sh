2195f7d261cde54747339adb9760cb5b68233ea4
Eggie-EPQ <59690399+Eggie-EPQ@users.noreply.github.com>
2020-01-13 14:48:50 +0800

    Update BBRv2.sh

--- BBRv2.sh
+++ BBRv2.sh
@@ -1,23 +1,23 @@
-apt -y update
-apt -y install git
-git clone https://github.com/Eggie-EPQ/EPQ.git
-cd BBRv2
-apt -y install *
-echo "tcp_bbr" >> /etc/modules
-echo "tcp_bbr2" >> /etc/modules
-echo "tcp_dctcp" >> /etc/modules
+yum -y install git
+git clone https://github.com/
+cd bbr2/centos
+yum -y localinstall *
+grub2-set-default 0
+echo "tcp_bbr" >> /etc/modules-load.d/tcp_bbr.conf
+echo "tcp_bbr2" >> /etc/modules-load.d/tcp_bbr2.conf
+echo "tcp_dctcp" >> /etc/modules-load.d/tcp_dctcp.conf
 sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_ecn/d' /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control = bbr2" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_ecn = 1" >> /etc/sysctl.conf
-echo 1 > /sys/module/tcp_bbr2/parameters/ecn_enable
-sed -i "/\/sys\/module\/tcp_bbr2\/parameters\/ecn_enable/d" /etc/rc.local
-add_to_rc.local "echo 1 > /sys/module/tcp_bbr2/parameters/ecn_enable"
 sysctl -p
+echo 1 > /sys/module/tcp_bbr2/parameters/ecn_enable
 rm -rf ~/bbr2
 read -p "Installation has finished.Restart？[Y/n]:" yn
 [ -z "${yn}" ] && yn="y"
 if [[ $yn == [Yy] ]]; then
+      echo -e "restarting"
+		  reboot
 	echo -e "restarting"
 	reboot
 fi
