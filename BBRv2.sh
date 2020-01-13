3a0b699c4eb3100150952c4fee11788f5fe775a4
Eggie-EPQ <59690399+Eggie-EPQ@users.noreply.github.com>
2020-01-13 09:42:04 +0800

    Update BBRv2.sh

--- BBRv2.sh
+++ BBRv2.sh
@@ -1,11 +1,11 @@
-yum -y install git
+apt -y update
+apt -y install git
 git clone https://github.com/xiya233/bbr2.git
-cd bbr2/centos
-yum -y localinstall *
-grub2-set-default 0
-echo "tcp_bbr" >> /etc/modules-load.d/tcp_bbr.conf
-echo "tcp_bbr2" >> /etc/modules-load.d/tcp_bbr2.conf
-echo "tcp_dctcp" >> /etc/modules-load.d/tcp_dctcp.conf
+cd bbr2/debian
+apt -y install *
+echo "tcp_bbr" >> /etc/modules
+echo "tcp_bbr2" >> /etc/modules
+echo "tcp_dctcp" >> /etc/modules
 sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_ecn/d' /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control = bbr2" >> /etc/sysctl.conf
@@ -15,9 +15,9 @@ sed -i "/\/sys\/module\/tcp_bbr2\/parameters\/ecn_enable/d" /etc/rc.local
 add_to_rc.local "echo 1 > /sys/module/tcp_bbr2/parameters/ecn_enable"
 sysctl -p
 rm -rf ~/bbr2
-read -p "Installation has been finished.Restart？[Y/n] :" yn
+read -p "Installation has finished.Restart？[Y/n]:" yn
 [ -z "${yn}" ] && yn="y"
 if [[ $yn == [Yy] ]]; then
-      echo -e "restarting"
-		  reboot
+	echo -e "restarting"
+	reboot
 fi
